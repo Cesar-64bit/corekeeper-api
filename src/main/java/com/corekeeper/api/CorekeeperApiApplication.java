c519399f05607fed9f7654053382c43437b2d780
Cesar Oswaldo Bernal Sanchez <cesar.bernal.ti@gmail.com>
2025-02-23 21:42:19 -0700

    Project Init

---
+++ src/main/java/com/corekeeper/api/CorekeeperApiApplication.java
@@ -0,0 +1,13 @@
+package com.corekeeper.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorekeeperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorekeeperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corekeeper/api/CorekeeperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corekeeper.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorekeeperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
